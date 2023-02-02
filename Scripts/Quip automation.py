b95a7ae932a6fecd12b2c1c7f894db307e5e6d59
Stealinglight <stealinglight@gmail.com>
2023-02-02 00:09:31 -0800

    Idea flow

---
+++ Scripts/Quip automation.py
@@ -0,0 +1,104 @@
+
+import pandas as pd
+import requests
+
+# Replace with your Quip API key
+api_key = "YOUR_API_KEY"
+
+# Replace with the ID of the sheet you want to export
+sheet_id = "SHEET_ID"
+
+# Build the URL for the export endpoint
+url = f"https://platform.quip.com/1/sheets/{sheet_id}/export/csv?access_token={api_key}"
+
+try:
+    # Make the GET request to export the sheet
+    response = requests.get(url)
+
+    # Check that the request was successful
+    if response.status_code == 200:
+        # Save the CSV data to a file
+        with open("sheet.csv", "wb") as f:
+            f.write(response.content)
+        print("Sheet exported successfully")
+        df = pd.read_csv('sheet.csv')
+    else:
+        raise Exception(f"Error exporting sheet: {response.status_code}")
+except Exception as e:
+    print(e)
+
+# ----------------------------------- #
+
+# Load the CSV file into a Pandas dataframe
+df = pd.read_csv('quip_sheet.csv')
+
+# Perform data aggregations or transformations
+aggregated_df = df.groupby(['column1', 'column2']).agg({'column3': 'sum'})
+
+# Save the aggregated data as a new CSV file
+aggregated_df.to_csv('aggregated_data.csv', index=False)
+
+# Upload the new CSV file to an S3 bucket
+s3 = boto3.client('s3')
+s3.upload_file('aggregated_data.csv', 'my-bucket', 'aggregated_data.csv')
+
+# ----------------------------------- #
+
+# Read the first CSV into a dataframe
+df1 = pd.read_csv("file1.csv")
+
+# Read the second CSV into a dataframe
+df2 = pd.read_csv("file2.csv")
+
+# Merge the dataframes on a common column, such as "IP_Address"
+merged_df = pd.merge(df1, df2, on="IP_Address", how="left")
+
+# Fill any NaN values with data from df2
+merged_df.fillna(df2)
+
+# Save the merged dataframe to a new CSV file
+merged_df.to_csv("merged.csv", index=False)
+
+# Upload the new CSV file to an S3 bucket
+s3 = boto3.client('s3')
+s3.upload_file('merged_data.csv', 'my-bucket', 'merged_data.csv')
+
+# ----------------------------------- #
+
+# Read the first CSV file into a dataframe
+df1 = pd.read_csv("file1.csv")
+
+# Read the second CSV file into a dataframe
+df2 = pd.read_csv("file2.csv")
+
+# Extract the cell values you want from df1
+values_to_update = df1.loc[0, 'column_name']
+
+# Update the corresponding cells in df2
+df2.loc[df2['column_name'] == 'matching_value', 'column_name'] = values_to_update
+
+# Save the updated dataframe to a new CSV file
+df2.to_csv("updated_file2.csv", index=False)
+
+# Note that this example assumes that you want to update cells in df2 based on a matching value in a column #
+
+# Load the first CSV file into a pandas DataFrame
+df1 = pd.read_csv('file1.csv')
+
+# ----------------------------------- #
+
+# Filter the data in the first DataFrame based on a condition
+filtered_df = df1[df1['column_name'] == some_value]
+
+# Get the number of rows in the filtered DataFrame (excluding the header)
+row_count = filtered_df.shape[0] - 1
+
+# Load the second CSV file into a pandas DataFrame
+df2 = pd.read_csv('file2.csv')
+
+# Update a cell in the second DataFrame with the row count
+df2.at[row_index, 'column_name'] = row_count
+
+# Save the updated second DataFrame to a new CSV file
+df2.to_csv('file3.csv', index=False)
+
